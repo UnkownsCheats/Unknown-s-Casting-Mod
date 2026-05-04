448483c72bb5eadd4fa7759e96e42812b875eb4c
UNKNOWNツ <UNKNOWNV5@hotmail.com>
2026-05-04 20:11:24 +0200

    Add files via upload

---
+++ Keybinds/GuiKeybinds.cs
@@ -0,0 +1,16 @@
+using UnityEngine.InputSystem;
+
+namespace UnknownCasting.Core.Keybinds
+{
+    public class GuiKeybinds
+    {
+        public static Key GuiKey = Key.Tab;
+        public static Key FirstPersonKey = Key.F;
+        public static Key FollowPlayerKey = Key.G;
+        public static Key CreateNestKey = Key.N;
+        public static Key DestroyNestKey = Key.M;
+        public static Key ViewCycleKey = Key.F;
+        public static Key MicToggleKey = Key.T;
+        public static Key ViewChangerKey = Key.F; // View changer key (cycles Third Person -> Front Third -> First Person)
+    }
+}
